d240d9094c8851447c70ef15a5d8e56d2a02c90b
DKelley <dianna.kelley@gmail.com>
2018-01-23 18:41:36 -0800

    Add project files.

---
+++ MyFirstChallengeAgain/Default.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MyFirstChallengeAgain
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string age = ageTextBox.Text;
+            decimal money = decimal.Parse(moneyTextBox.Text);
+            if (money >= 50)
+            {
+              string result = String.Format("Well then,  {0} years old and {1:C} in your pocket, you should enjoy a night out.", age, money);
+                resultLabel.Text = result;
+            }
+
+            else if ((money>= 10) && (money<50))
+            {
+                string result = String.Format("It's pretty good to have {1:C} in your pocket at {0} years old.", age, money);
+                resultLabel.Text = result;
+               }
+
+            else
+
+            {
+                string result = String.Format("At {0} years old I would have expected you to have more than {1:C} in your pocket", age, money);
+                resultLabel.Text = result;
+            }
+            
+          
+        }
+
+    
+
+
+    }
+}
